f01922f3fcd9c253e2b29b2df6d2e6de948737fe
Scot Brown <scotb38@gmail.com>
2018-08-28 20:56:56 -0400

    Bump to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject weaver "0.1.1"
+(defproject weaver "0.1.2-SNAPSHOT"
   :description "Configuration templating DSL"
   :url "https://github.com/SVMBrown/weaver"
   :license {:name "Eclipse Public License"
